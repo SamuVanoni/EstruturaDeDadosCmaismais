c89abab5d5184fd0b7e714b73d2724ad69b2e5fa
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-09-27 21:15:03 -0300

    :sparkles: feat: Add desafio 02 lista de revisao

---
+++ ListaRevisao/q2/q2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x;
+    int vet1[x];
+    for(int i = 0; i < x; i++){
+        cin >> vet1[i];
+    }
+
+    cin >> y;
+    int vet2[y];
+    for(int i = 0; i < y; i++){
+        cin >> vet2[i];
+    }
+
+    int vet3[x + y];
+    for(int i = 0; i < x; i++){
+        vet3[i] = vet1[i];
+    }
+    for(int i = 0; i < y; i++){
+        bool sentinel = true;
+        for(int j = 0; j < x; j++){
+            if(vet2[i] == vet3[j]){
+                sentinel = false;
+            }
+        }
+        if(sentinel){
+            vet3[x] = vet2[i];
+            x++;
+        }
+    }
+
+    for(int i = 0; i < x; i++){
+        cout << vet3[i] << " ";
+    }
+
+    return 0;
+}
