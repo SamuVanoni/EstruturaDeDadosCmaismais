a0001d5deec228bcfe16852e4f81366a09d099ff
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-09-30 09:30:32 -0300

    :sparkles: feat: Add alocação dinamica desafio 2 lista de revisao

--- src/lista_revisao/q02/q2.cpp
+++ src/lista_revisao/q02/q2.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int *geraVetor(int n){
+    int *V = new int[n];
+    return V;
+}
+
 int main(){
     int x, y;
     cin >> x;
-    int vet1[x];
+    int *vet1 = geraVetor(x);
     for(int i = 0; i < x; i++){
         cin >> vet1[i];
     }
 
     cin >> y;
-    int vet2[y];
+    int *vet2 = geraVetor(y);
     for(int i = 0; i < y; i++){
         cin >> vet2[i];
     }
 
-    int vet3[x + y];
+    int *vet3 = geraVetor(x + y);
     for(int i = 0; i < x; i++){
         vet3[i] = vet1[i];
     }
@@ -35,6 +40,10 @@ int main(){
     for(int i = 0; i < x; i++){
         cout << vet3[i] << " ";
     }
+
+    delete[] vet1;
+    delete[] vet2;
+    delete[] vet3;
 
     return 0;
 }
